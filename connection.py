a22575f8e529d21219b5e59f71e914588570172c
datnguyen <dat.2010.50@gmail.com>
2026-08-16 13:49:52 -0400

    update: add back send/receive functions

--- connection.py
+++ connection.py
@@ -1,23 +1,16 @@
 import socket
+import struct
+import json
+from protocol import build_header, MessageType
 
 
-def create_tcp_socket(host, port):
-    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def create_listening_socket(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    tcp_socket.bind((host, port))
-    tcp_socket.listen()
+    sock.bind((host, port))
+    sock.listen()
 
-    return tcp_socket
-
-
-def create_udp_socket(host, port):
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    udp_socket.bind((host, port))
-
-    return udp_socket
+    return sock
 
 
 def connect(host, port, timeout):
@@ -42,3 +35,57 @@ def close_connection(connection):
         pass
 
     connection.close()
+    
+
+def ping(connection):
+    send(connection, MessageType.PING, None)
+
+
+def pong(connection):
+    send(connection, MessageType.PONG, None)
+    
+    
+def send(connection, message_type, session_id, payload=None):
+    payload = payload if payload is not None else b''
+    header = build_header(message_type, session_id, len(payload))
+
+    connection.sendall(header + payload)
+
+
+# def send_stream(connection, header, chunks, size):
+#     header['size'] = size
+#     framed_header = build_header(header)
+#
+#     connection.sendall(framed_header)
+#
+#     for chunk in chunks:
+#         connection.sendall(chunk)
+
+
+def receive(connection):
+    header_size_bytes = _receive_exactly(connection, 4)
+    header_size, = struct.unpack('!I', header_size_bytes)
+
+    header_bytes = _receive_exactly(connection, header_size)
+    header_json = header_bytes.decode('utf-8')
+    header = json.loads(header_json)
+
+    payload = _receive_exactly(connection, header['payload_length'])
+
+    return header, payload
+
+
+def _receive_exactly(connection, size):
+    data = bytearray()
+
+    while len(data) < size:
+        remaining = size - len(data)
+        chunk = connection.recv(remaining)
+
+        if chunk is None:
+            return None
+
+        data.extend(chunk)
+
+    return bytes(data)
+
