401df5730a7bef39adddb639b26ef3564ad73498
datnguyen <dat.2010.50@gmail.com>
2026-08-17 15:23:53 -0400

    update: add compatibility for streaming messaged

--- protocol.py
+++ protocol.py
@@ -6,6 +6,7 @@ from enum import StrEnum
 class MessageType(StrEnum):
     HELLO = 'HELLO'
     HELLO_ACK = 'HELLO_ACK'
+    HELLO_DONE = 'HELLO_DONE'
 
     PING = 'PING'
     PONG = 'PONG'
@@ -13,7 +14,8 @@ class MessageType(StrEnum):
     TEXT = 'TEXT'
     DATA = 'DATA'
     FILE = 'FILE'
-    STREAM_CHUNK = 'STREAM_CHUNK'
+    STREAM_START = 'STREAM_START'
+    STREAM_END = 'STREAM_END'
 
     ERROR = 'ERROR'
 
@@ -24,15 +26,25 @@ def handle_packet():
     pass
 
 
-def build_header(message_type, connection_id, payload_length):
+def build_header(message_type, payload_length, connection_id, stream_id=None):
     header = {
-        'message_type': message_type.value,
-        'session_id': connection_id,
-        'payload_length': payload_length
+        'type': message_type.value,
+        'length': payload_length,
+        'connection_id': connection_id,
+        'stream_id': stream_id,
     }
 
     header_json = json.dumps(header)
     header_bytes = header_json.encode('utf-8')
-    header_size_bytes = struct.pack('!I', len(header_bytes))
+    header_size = struct.pack('!I', len(header_bytes))
 
-    return header_size_bytes + header_bytes
+    return header_size + header_bytes
+
+
+def parse_header(header_bytes):
+    header_json = header_bytes.decode('utf-8')
+    header = json.loads(header_json)
+
+    header['type'] = MessageType(header['type'])
+
+    return header
